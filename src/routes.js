7c320a03c68323f8e9b25a20d5faf6a1c75d1f51
Julia <JuliaAmaralSantos@outlook.com>
2024-04-21 14:49:49 -0300

    preparei o ambiente para começar o desenvolvimento

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+//esse arquivo organiza as rotas que vamos ter dentro da API
+//caminho para consultar todos os dados, inserir os dados, alterar os dados, excluir os dados
+
+//express é um framework para facilitar o desenvolvimento de aplicacoes backend
+//chamando o express para utilizar no arquivo routes.
+
+const express = require('express');
+
+const router = express.Router();
